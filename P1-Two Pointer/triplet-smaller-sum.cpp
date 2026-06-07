8da6d592bd12f0948890d123a626fd213024c72e
Vinit <laddhavinit99@gmail.com>
2026-06-08 00:09:18 +0530

    Triplet closestsmaller sum

--- P1-Two Pointer/triplet-smaller-sum.cpp
+++ P1-Two Pointer/triplet-smaller-sum.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    
+    vector<int> v={3,1,5,4,7};
+    int ans=0;
+    int target=12;
+    sort(v.begin(),v.end());
+    for(int i=0;i<v.size()-2;i++){
+        int j=i+1,k=v.size()-1;
+        while(j<k){
+            if(v[i]+v[j]+v[k]<target){
+                ans+=k-j;
+                j++;
+            }
+            else{
+                k--;
+            }
+        }
+    }
+    cout<<ans;
 return 0;
 }
