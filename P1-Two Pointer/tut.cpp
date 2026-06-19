9c7c0bb563ac009b2197e52384c3553720a28664
Vinit <laddhavinit99@gmail.com>
2026-06-19 23:16:47 +0530

    Smallest Subarray with sum>k

---
+++ P1-Two Pointer/tut.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int func(vector<int> v, int k){
+    int i=0,j=0;
+    int ans=INT_MAX;
+    int sum=0;
+    while(j<v.size()){
+        sum+=v[j];
+        while(sum>k){
+            ans=min(ans, j-i+1);
+            sum-=v[i];
+            i++;
+        }
+        j++;
+    }
+    return ans;
+}
+int main(){
+    vector<int> v={2, 3, 1, 2, 4, 3};
+    int target=7;
+    cout<<func(v,target);
+    return 0;
+}
