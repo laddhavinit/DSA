916900f6976d8a5f4184d3dda139d739e600b9c3
Vinit <laddhavinit99@gmail.com>
2026-06-27 01:39:59 +0530

    String with concatenation of all words

---
+++ P2- Sliding Window/substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void substring(string s, vector<string> words, vector<int> &ans){
+    unordered_map<string,int> m1;
+    int wl= words[0].size();
+    int wc= words.size();
+    int req=wl*wc;
+    for(int i=0;i<words.size();i++){
+        m1[words[i]]++;
+    }
+    for (int i = 0; i < wl; i++)
+    {
+        int start=i;
+        unordered_map<string, int> m2;
+        for(int j=i;j<=s.size()-wl;j+=wl){
+            string helper=s.substr(j,wl);
+            if(m1.count(helper)>0){
+                m2[helper]++;
+                if(m1==m2){
+                    ans.push_back(start);
+                    m2[s.substr(start,wl)]--;
+                    start+=wl;
+                }
+            }
+            else{
+                m2.clear();
+                start=j+wl;
+            }
+        }
+    }
+    
+}
+
+int main(){
+    vector<string> words = {"foo","bar"};
+    string s="barfoothefoobarman";
+    vector<int> ans;
+    substring(s,words, ans);
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    return 0;
+}
